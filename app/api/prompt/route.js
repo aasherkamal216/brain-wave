5ac9edf82c612150b718643f24efb02e230827b6
aasherkamal216 <aasherkamal786@gmail.com>
2024-10-21 18:43:55 +0500

    updated

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,18 +2,18 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const GET = async (request) => {
-
     try {
-        await connectToDB();
-
-        const prompts = await Prompt.find({}).populate('creator');
-
-        return new Response(JSON.stringify(prompts), { status: 200 });
-
+      await connectToDB();
+      const prompts = await Prompt.find({}).populate('creator');
+      return new Response(JSON.stringify(prompts), { 
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0',
+        },
+      });
     } catch (error) {
-
-        return new Response("Failed to fetch all prompts", { status: 500 });
-        
+      return new Response("Failed to fetch all prompts", { status: 500 });
     }
-
-}
+  }
--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
-  }
-  
-  export default nextConfig;
-  
+  // Enable the app directory feature directly
+  appDir: true,
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Allow images from this domain
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true, // Enable top-level await in Webpack
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
