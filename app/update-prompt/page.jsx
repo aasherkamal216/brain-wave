d11b18306c8e50771bc76a38a70eae75ece3b105
aasherkamal216 <aasherkamal786@gmail.com>
2024-10-21 15:27:23 +0500

    updated

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-
 import Form from "@components/Form";
 
 const UpdatePrompt = () => {
@@ -10,7 +9,7 @@ const UpdatePrompt = () => {
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
-  const [post, setPost] = useState({ prompt: "", tag: "", });
+  const [post, setPost] = useState({ prompt: "", tag: "" });
   const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
@@ -54,7 +53,7 @@ const UpdatePrompt = () => {
 
   return (
     <Form
-      type='Edit'
+      type="Edit"
       post={post}
       setPost={setPost}
       submitting={submitting}
@@ -63,4 +62,10 @@ const UpdatePrompt = () => {
   );
 };
 
-export default UpdatePrompt;
+export default function UpdatePromptWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UpdatePrompt />
+    </Suspense>
+  );
+}
